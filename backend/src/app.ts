4641c3eeaad31d474d2da21dca86195a3d1abd24
adrian <acalerobarrero@gmail.com>
2022-06-24 16:56:34 +0200

    Init files for proyect

---
+++ backend/src/app.ts
@@ -0,0 +1,46 @@
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+// import path from 'path'
+
+import homeRouter from './router/home.js';
+import { taskRouter } from './router/task.js';
+import { notesRouter } from './router/notes.js';
+import { bookRouter } from './router/book.js';
+
+export const app = express();
+
+// Middlewares
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+export interface ExtraRequest extends Request {
+    calculo: number;
+}
+
+app.use((req, res, next) => {
+    req;
+    res;
+    console.log('Soy un middlewares');
+    (req as ExtraRequest).calculo = 47_638;
+    next();
+});
+
+app.use('/', homeRouter);
+app.use('/task', taskRouter);
+app.use('/notes', notesRouter);
+app.use('/books', bookRouter);
+
+app.use((error: Error, req: Request, resp: Response, next: NextFunction) => {
+    req;
+    next;
+    let status = 500;
+    if (error.name === 'ValidationError') {
+        status = 406;
+    }
+    resp.status(status);
+    const result = { status: status, error: error.message };
+    resp.end(JSON.stringify(result));
+});
