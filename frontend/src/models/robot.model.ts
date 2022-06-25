d5262ecdb12fe6ec34322eacd81fb7d53bd2854f
adrian <acalerobarrero@gmail.com>
2022-06-25 12:39:26 +0200

    Add model robot

---
+++ frontend/src/models/robot.model.ts
@@ -0,0 +1,9 @@
+export class RobotModel {
+  constructor(
+    public name: string,
+    public img: string,
+    public velocity: number,
+    public resistence: number,
+    public date: string
+  ) {}
+}
