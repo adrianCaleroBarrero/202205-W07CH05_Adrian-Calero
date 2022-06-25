337a678fca9ddee90d8eab58d5265cfdcf633aba
adrian <acalerobarrero@gmail.com>
2022-06-25 18:47:32 +0200

    Add form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import { Form } from "./components/form/form";
 import { List } from "./components/list/list";
 
 function App() {
   return (
     <>
       <h1>Robots</h1>
+      <Form />
       <List />
     </>
   );
