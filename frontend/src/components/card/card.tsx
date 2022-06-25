0c226c41986bb33e42fbaf6434874b316d79c952
adrian <acalerobarrero@gmail.com>
2022-06-25 18:47:57 +0200

    Add delete button & and functionality

--- frontend/src/components/card/card.tsx
+++ frontend/src/components/card/card.tsx
@@ -1,29 +1,41 @@
-import { CardContent, CardMedia } from "@mui/material";
+import { Button, CardContent, CardMedia } from "@mui/material";
 import Card from "@mui/material/Card";
 import { blue } from "@mui/material/colors";
+import { useDispatch } from "react-redux";
+import * as ac from "../../redux/action.creators";
 
 import { iRobot } from "../../interfaces/robot";
+import { HttpStoreRobot } from "../../services/http.storage";
 
 export function CardList({ robot }: { robot: iRobot }) {
+  const api = new HttpStoreRobot();
+  const dispatch = useDispatch();
+
+  function HandleDelete() {
+    console.log(robot);
+    api
+      .deleteRobot(robot)
+      .then((resp) => dispatch(ac.deleteRobotAction(robot)));
+  }
   return (
     <li>
-      <Card sx={{ maxWidth: 345 }}>
+      <Card sx={{ maxWidth: 345 }} style={{ borderRadius: 20 }}>
         <CardMedia
           component="img"
           height="340"
           image={robot.img}
           alt={robot.name}
         />
-        <CardContent style={{ backgroundColor: blue[400] }}>
+        <CardContent style={{ backgroundColor: blue[100] }}>
           <p>Name: {robot.name}</p>
           <p>Date: {robot.date}</p>
           <p>Velocity: {robot.velocity}</p>
-          <p>Resistance: {robot.resistence}</p>
+          <p>Resistance: {robot.resistence}</p>{" "}
+          <Button variant="contained" color="error" onClick={HandleDelete}>
+            Delete
+          </Button>
         </CardContent>
       </Card>
     </li>
   );
-}
-function useStyles(arg0: { color: string }) {
-  throw new Error("Function not implemented.");
 }
