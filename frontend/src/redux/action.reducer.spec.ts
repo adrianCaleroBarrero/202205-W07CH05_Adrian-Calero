3ada610125a74b60b6fa3debac08665043b80bec
adrian <acalerobarrero@gmail.com>
2022-06-26 19:17:12 +0200

    Change id for _id in reducer test

--- frontend/src/redux/action.reducer.spec.ts
+++ frontend/src/redux/action.reducer.spec.ts
@@ -27,7 +27,7 @@ describe("Given the RobotReducer", () => {
   describe("When use addRobotAction", () => {
     test("Then it should render 2 mocks", () => {
       const initialState: iRobot[] = [mockRobot];
-      const mockRobotAdd = { ...mockRobot, id: "2", name: "test2" };
+      const mockRobotAdd = { ...mockRobot, _id: "2", name: "test2" };
       const newState = robotReducer(
         initialState,
         ac.addRobotAction(mockRobotAdd)
@@ -41,7 +41,7 @@ describe("Given the RobotReducer", () => {
   describe("When use modifyRobotAction", () => {
     test("Then it should render modifymock", () => {
       const initialState: iRobot[] = [mockRobot];
-      const mockRobotModify = { ...mockRobot, id: "1", name: "test2" };
+      const mockRobotModify = { ...mockRobot, _id: "1", name: "test2" };
       const newState = robotReducer(
         initialState,
         ac.modifyRobotAction(mockRobotModify)
@@ -53,7 +53,7 @@ describe("Given the RobotReducer", () => {
 
     test("Then it should render not modifymock", () => {
       const initialState: iRobot[] = [mockRobot];
-      const mockRobotModify = { ...mockRobot, id: "3", name: "test2" };
+      const mockRobotModify = { ...mockRobot, _id: "3", name: "test2" };
       const newState = robotReducer(
         initialState,
         ac.modifyRobotAction(mockRobotModify)
