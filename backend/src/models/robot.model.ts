7856c672bdf56b579dab5597458f6f550187bacc
adrian <acalerobarrero@gmail.com>
2022-06-27 18:50:29 +0200

    Add param owner for relationship

--- backend/src/models/robot.model.ts
+++ backend/src/models/robot.model.ts
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
-import { mongooseConnect } from '../db/mongoose.js';
+import { mongooseConnect, RelationField } from '../db/mongoose.js';
 
 await mongooseConnect();
 
 export interface iRobot {
     name: string;
     img: string;
+    owner: RelationField;
     velocity: number;
     resistence: number;
     date: string;
@@ -17,6 +18,10 @@ const robotSchema = new mongoose.Schema({
     velocity: mongoose.SchemaTypes.Number,
     resistence: mongoose.SchemaTypes.Number,
     date: mongoose.SchemaTypes.String,
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    },
 });
 
 export const RobotModel = mongoose.model('robot', robotSchema);
