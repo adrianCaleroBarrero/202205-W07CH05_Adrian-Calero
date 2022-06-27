27aedf623d8f395cbb7c88bd46587a190e14be73
adrian <acalerobarrero@gmail.com>
2022-06-27 18:26:03 +0200

    Add error to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 
@@ -11,3 +11,25 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/robots', robotRouter);
+app.use('/robot', robotRouter);
+// app.use('/user', userRouter);
+// app.use('/users', userRouter);
+
+app.use((error: Error, req: Request, resp: Response, next: NextFunction) => {
+    req;
+    next;
+    console.log(error.message);
+    let status = 500;
+    if (error.name === 'ValidationError') {
+        status = 406;
+    } else {
+        //
+    }
+    resp.status(status);
+    const result = {
+        status: status,
+        type: error.name,
+        error: error.message,
+    };
+    resp.send(JSON.stringify(result));
+});
