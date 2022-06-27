9f8fb2d483a036033b51aa2122f414dec7946a2d
adrian <acalerobarrero@gmail.com>
2022-06-27 18:26:52 +0200

    Add model of users

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import { mongooseConnect, RelationField } from '../db/mongoose.js';
+
+await mongooseConnect();
+
+export interface iUser {
+    id: string;
+    name: string;
+    email: string;
+    robots: Array<RelationField>;
+}
+
+const userSchema = new mongoose.Schema({
+    name: { type: mongoose.SchemaTypes.String, required: true },
+    email: mongoose.SchemaTypes.String,
+    tasks: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'Task',
+        },
+    ],
+});
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        delete returnedObject.__v;
+    },
+});
+
+export const User = mongoose.model('User', userSchema);
